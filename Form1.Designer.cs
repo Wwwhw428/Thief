d621c23a6b6ff1cc7a390ab2b7577d31d0089a81
Wwwhw428 <wanghaowen28@gmail.com>
2023-03-26 10:39:10 +0800

    feat: 实现隐藏窗口、透明度调节、listbox选择应用程序、增加图标和托盘退出

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,39 +32,132 @@ namespace Thief
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Toggle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(10, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 148);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 20);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 250);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(10, 280);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(10, 310);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 20);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "隐藏快捷键";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(220, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "降低透明度快捷键";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(220, 310);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 20);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "提高透明度快捷键";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // trayMenu
+            // 
+            this.trayMenu.Name = "trayMenu";
+            this.trayMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.ContextMenuStrip = this.trayMenu;
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.trayMenu.Items.Add("Exit", null, Exit_Click);
+            this.trayIcon.Visible = true;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(340, 315);
-            this.Name = "Form1";
-            this.ResumeLayout(false);
-
-            this.button1 = new Button();
-
-            this.button1.Text = "Toggle";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new EventHandler(this.button1_Click);
-            this.button1.KeyUp += new KeyEventHandler(this.button1_KeyUp);
-
+            this.ClientSize = new System.Drawing.Size(320, 350);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "App Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-            this.trayMenu = new ContextMenuStrip();
-            this.trayMenu.Items.Add("Exit", null, Exit_Click);
-
-            //this.trayIcon = new NotifyIcon();
-            //this.trayIcon.Icon = new Icon("AppIcon.ico"); // 使用适当的图标文件
-            //this.trayIcon.ContextMenuStrip = this.trayMenu;
-            //this.trayIcon.Visible = true;
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnClickThis;
-        private System.Windows.Forms.Label lbTest;
-        private Button button1;
-        private ContextMenuStrip trayMenu;
-        private NotifyIcon trayIcon;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,8 @@ using NHotkey;
 using NHotkey.WindowsForms;
 using Gma.System.MouseKeyHook;
 using System.Windows.Input;
+using System.Text;
+using System.Collections.Generic;
 
 namespace Thief
 {
@@ -39,27 +41,90 @@ namespace Thief
 
         [DllImport("user32.dll")]
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetShellWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
         #endregion 
+
+        private const int HOTKEY_ID_1 = 1;
+        private const int HOTKEY_ID_2 = 2;
+        private const int HOTKEY_ID_3 = 3;
+
+        private const uint MOD_ALT = 0x0001;
+        private const uint LWA_ALPHA = 0x00000002;
 
         private const int GWL_EXSTYLE = -20;
         private const int WS_EX_LAYERED = 0x80000;
-        private const int LWA_ALPHA = 0x2;
-
-        private IKeyboardMouseEvents _mouseHook;
-
-        // 存储用户选择的按键和修饰符
-        public Keys ShortcutKey { get; private set; }
-        public ModifierKeys ShortcutModifiers { get; private set; }
 
         private const int SW_HIDE = 0;
         private const int SW_SHOW = 5;
+
+        private Button button1, button2, button3, button4;
+        private TextBox textBox1, textBox2, textBox3;
+        private Label label1;
+        private ListBox listBox1;
+        private NotifyIcon trayIcon;
+        private ContextMenuStrip trayMenu;
+
+        private IntPtr selectedWindowHandle = IntPtr.Zero;
+
         public Form1()
         {
             // 初始化窗体组件
             InitializeComponent();
 
-            _mouseHook = Hook.GlobalEvents();
-            _mouseHook.MouseWheel += MouseHook_MouseWheel;
+            FillWindowList();
+            listBox1.SelectedIndexChanged += new EventHandler(this.listBox1_SelectedIndexChanged);
+            this.KeyPreview = true;
+            this.KeyUp += new KeyEventHandler(this.Form1_KeyUp);
+            textBox1.Text = "C";
+            textBox2.Text = "Z";
+            textBox3.Text = "X";
+
+            RegisterHotKey(this.Handle, HOTKEY_ID_1, MOD_ALT, (uint)Keys.C);
+            RegisterHotKey(this.Handle, HOTKEY_ID_2, MOD_ALT, (uint)Keys.Z);
+            RegisterHotKey(this.Handle, HOTKEY_ID_3, MOD_ALT, (uint)Keys.X);
+
+            trayIcon.MouseDoubleClick += TrayIcon_MouseDoubleClick;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (listeningToHotkeys)
+            {
+                if (textBox1.Enabled == false)
+                {
+                    textBox1.Text = e.KeyCode.ToString();
+                    button2_Click(null, null);
+                }
+                else if (textBox2.Enabled == false)
+                {
+                    textBox2.Text = e.KeyCode.ToString();
+                    button3_Click(null, null);
+                }
+                else if (textBox3.Enabled == false)
+                {
+                    textBox3.Text = e.KeyCode.ToString();
+                    button4_Click(null, null);
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -67,79 +132,194 @@ namespace Thief
             ToggleWindowVisibility();
         }
 
-        private void button1_KeyUp(object sender, KeyEventArgs e)
+        private void ToggleWindowVisibility()
         {
-            if (e.KeyCode == Keys.F12)
+            if (selectedWindowHandle != IntPtr.Zero)
             {
-                ToggleWindowVisibility();
+                if (selectedWindowHandle != this.Handle)
+                {
+                    if (IsWindowVisible(selectedWindowHandle))
+                    {
+                        ShowWindow(selectedWindowHandle, SW_HIDE);
+                    }
+                    else
+                    {
+                        ShowWindow(selectedWindowHandle, SW_SHOW);
+                    }
+                }
             }
         }
 
-        private void ToggleWindowVisibility()
+        private static bool IsApplicationWindow(IntPtr hWnd)
         {
-            IntPtr hWnd = GetForegroundWindow();
-            if (hWnd != this.Handle)
+            if (hWnd == GetShellWindow()) return false;
+            if (!IsWindowVisible(hWnd)) return false;
+
+            int length = GetWindowTextLength(hWnd);
+            if (length == 0) return false;
+
+            return true;
+        }
+
+        private void FillWindowList()
+        {
+            listBox1.Items.Clear();
+
+            EnumWindowsProc enumWindowsProc = (hWnd, lParam) =>
             {
-                if (IsWindowVisible(hWnd))
+                if (!IsApplicationWindow(hWnd)) return true;
+
+                int length = GetWindowTextLength(hWnd);
+                if (length == 0) return true;
+
+                StringBuilder stringBuilder = new StringBuilder(length + 1);
+                GetWindowText(hWnd, stringBuilder, stringBuilder.Capacity);
+                string windowTitle = stringBuilder.ToString();
+
+                if (!string.IsNullOrEmpty(windowTitle))
                 {
-                    ShowWindow(hWnd, SW_HIDE);
+                    listBox1.Items.Add(new KeyValuePair<IntPtr, string>(hWnd, windowTitle));
+                    listBox1.DisplayMember = "Value";
+                    listBox1.ValueMember = "Key";
                 }
-                else
+
+                return true;
+            };
+
+            EnumWindows(enumWindowsProc, IntPtr.Zero);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                KeyValuePair<IntPtr, string> selectedItem = (KeyValuePair<IntPtr, string>)listBox1.SelectedItem;
+                label1.Text = selectedItem.Value;
+                selectedWindowHandle = selectedItem.Key;
+            }
+        }
+
+        private bool listeningToHotkeys = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!listeningToHotkeys)
+            {
+                listeningToHotkeys = true;
+                textBox1.Enabled = false;
+            }
+            else
+            {
+                listeningToHotkeys = false;
+                textBox1.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!listeningToHotkeys)
+            {
+                listeningToHotkeys = true;
+                textBox2.Enabled = false;
+            }
+            else
+            {
+                listeningToHotkeys = false;
+                textBox2.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!listeningToHotkeys)
+            {
+                listeningToHotkeys = true;
+                textBox3.Enabled = false;
+            }
+            else
+            {
+                listeningToHotkeys = false;
+                textBox3.Enabled = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+
+            if (m.Msg == WM_HOTKEY)
+            {
+                int id = m.WParam.ToInt32();
+                switch (id)
                 {
-                    ShowWindow(hWnd, SW_SHOW);
+                    case HOTKEY_ID_1:
+                        ToggleWindowVisibility();
+                        break;
+                    case HOTKEY_ID_2:
+                        // 提高透明度
+                        AdjustWindowTransparency(10);
+                        break;
+                    case HOTKEY_ID_3:
+                        // 降低透明度
+                        AdjustWindowTransparency(-10);
+                        break;
                 }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void AdjustWindowTransparency(int change)
+        {
+            if (selectedWindowHandle != IntPtr.Zero)
+            {
+                int exStyle = GetWindowLong(selectedWindowHandle, GWL_EXSTYLE);
+                SetWindowLong(selectedWindowHandle, GWL_EXSTYLE, exStyle | WS_EX_LAYERED);
+
+                // 首先检查窗口是否已设置为分层。
+                // 如果窗口尚未设置为分层（可能导致窗口突然变得全透明），将设置窗口为分层并将其透明度设置为100%。
+                // 然后再进行透明度调整。
+                if ((exStyle & WS_EX_LAYERED) != WS_EX_LAYERED)
+                {
+                    SetWindowLong(selectedWindowHandle, GWL_EXSTYLE, exStyle | WS_EX_LAYERED);
+                    SetLayeredWindowAttributes(selectedWindowHandle, 0, 255, LWA_ALPHA);
+                }
+
+                byte currentAlpha;
+                GetLayeredWindowAttributes(selectedWindowHandle, out uint colorKey, out currentAlpha, out uint flags);
+
+                int newAlpha = currentAlpha + change;
+                newAlpha = Math.Max(0, Math.Min(255, newAlpha));
+
+                SetLayeredWindowAttributes(selectedWindowHandle, 0, (byte)newAlpha, LWA_ALPHA);
+            }
+        }
+
+        private void TrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
             }
         }
 
         private void Exit_Click(object sender, EventArgs e)
         {
+            UnregisterHotKey(this.Handle, HOTKEY_ID_1);
+            UnregisterHotKey(this.Handle, HOTKEY_ID_2);
+            UnregisterHotKey(this.Handle, HOTKEY_ID_3);
+
+            trayIcon.Visible = false;
             Application.Exit();
-        }
-
-        private void HideWindow(object sender, HotkeyEventArgs e)
-        {
-            IntPtr hWnd = GetForegroundWindow();
-            if (hWnd != IntPtr.Zero)
-            {
-                if (IsWindowVisible(hWnd))
-                {
-                    this.WindowState = FormWindowState.Minimized;
-                }
-            }
-        }
-
-        private void MouseHook_MouseWheel(object sender, MouseEventArgs e)
-        {
-            if (Form.ModifierKeys == Keys.Alt)
-            {
-                IntPtr hWnd = GetForegroundWindow();
-                if (hWnd != IntPtr.Zero)
-                {
-                    int exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
-                    if ((exStyle & WS_EX_LAYERED) == 0) // 检查窗口是否已具有 WS_EX_LAYERED 样式
-                    {
-                        SetWindowLong(hWnd, GWL_EXSTYLE, exStyle | WS_EX_LAYERED);
-                        SetLayeredWindowAttributes(hWnd, 0, 255, LWA_ALPHA); // 设置窗口为不透明，避免初始全透明
-                    }
-
-                    int currentTransparency;
-                    GetLayeredWindowAttributes(hWnd, out uint _, out byte bAlpha, out uint _);
-                    currentTransparency = bAlpha;
-
-                    int delta = e.Delta > 0 ? 10 : -10; // 根据滚轮方向调整透明度
-                    int newTransparency = Math.Max(0, Math.Min(255, currentTransparency + delta));
-                    SetLayeredWindowAttributes(hWnd, 0, (byte)newTransparency, LWA_ALPHA);
-                }
-            }
-        }
-
-        // 在窗口关闭时注销全局热键
-        protected override void OnFormClosing(FormClosingEventArgs e)
-        {
-            base.OnFormClosing(e);
-
-            _mouseHook.MouseWheel -= MouseHook_MouseWheel;
-            _mouseHook.Dispose();
         }
     }
 }
